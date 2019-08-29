ff40c29852be2af3cfde7c56e670d9680e21d54e
NgoMinhNhi <ngominhnhi1998@gmail.com>
2019-08-29 22:23:40 +0700

    fix layout (/tieng-anh-tong-quat)

--- src/Pages/LandingPage/Banner/index.js
+++ src/Pages/LandingPage/Banner/index.js
@@ -4,13 +4,14 @@ import './styles.scss'
 class Banner extends Component {
     render() {
         return (
-            <section className="home lazy-load" id="home">
+            <div className="wrap-banner-box">
+            <section className="home lazy-load">
                 <div className="home__content container">
                     <div className="row">
                         <div className="col-sm-7 ">
                             <h1 className="wrap-banner-title banner__title">Tiếng Anh Tổng Quát</h1>
                         </div>
-                        <div className=" col-sm-5 wrap-banner-img">
+                        <div className="col-sm-5 wrap-banner-img">
                             <div className="wrap-img">
                                 <img className="banner-img" src={generalImage} alt="Tiếng Anh tổng quát" />
                             </div>
@@ -20,6 +21,7 @@ class Banner extends Component {
                 <div className="home__bg home__bg--center"></div>
                 <div className="home__bg home__bg--bottom"></div>
             </section>
+            </div>
         );
     }
 }
