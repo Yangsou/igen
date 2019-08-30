69583d510503b954324ac8464e4ce176b33512d4
yangsou <namnguyentrung95@gmail.com>
2019-08-30 10:36:22 +0700

    fix bugs stylesheet

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,8 +15,8 @@ class Home extends React.Component {
         <div className="home__bg home__bg--people"></div>
 
         <div className="home__content container">
-          <p className="home__content__brand">I-Gen</p>
-          <p className="home__content__text">i-Gen  Một triệu sinh viên Việt Nam tự tin với Tiếng Anh</p>
+          <p className="home__content__brand">i-Gen</p>
+          <p className="home__content__text">Một triệu sinh viên Việt Nam tự tin với Tiếng Anh</p>
           <button onClick={this.clickPrimaryButton} className="btn btn--gradient btn--radius">Học tập ngay</button>
         </div>
       </section>
