243fb544af42285e00d901f329fe843102bc8297
NgoMinhNhi <ngominhnhi1998@gmail.com>
2019-09-16 14:54:25 +0700

    add Result page

--- src/components/Breakcrumb/index.js
+++ src/components/Breakcrumb/index.js
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 import './styles.scss';
 class Breadcrumb extends Component {
     render() {
-      const { data } = this.props;
+      const { data, setBackground } = this.props;
         return (
-            <nav className="container">
-                <ol className="breadcrumb">
-                  {
-                    data.map((e, index) => (
-                      <li key={index} className={`breadcrumb-item ${e.active ? 'active' : ''}`}>
-                        {
-                          e.path ? <Link to="/">{e.label}</Link>
-                            : e.label
-                        }
-                        
-                      </li>
-                    ))
-                  }
-                </ol>
-            </nav>
+          <section className={`${setBackground?'background-grey':''} container-fluid`}>
+              <nav className="container">
+                  <ol className="breadcrumb">
+                    {
+                      data.map((e, index) => (
+                        <li key={index} className={`breadcrumb-item ${e.active ? 'active' : ''}`}>
+                          {
+                            e.path ? <Link to={e.path}>{e.label}</Link>
+                              : e.label
+                          }
+                          
+                        </li>
+                      ))
+                    }
+                  </ol>
+              </nav>
+            </section>
         );
     }
 }
